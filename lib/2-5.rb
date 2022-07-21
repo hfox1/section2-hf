259ecd43c4d0863dfc367d872172506d3bf5b6e9
Harry Fox <49591810+hfox1@users.noreply.github.com>
2022-07-22 01:26:51 +0200

    only chunk loop left to finish"

--- lib/2-5.rb
+++ lib/2-5.rb
@@ -2,35 +2,43 @@ class DiaryEntry
   def initialize(title, contents) # title, contents are strings
     @title = title
     @contents = contents 
+    @start_at_word = 0
+    fail "Please enter text" if (@title != @title.to_s)
+    fail "Please add a title" if @title.empty?
+    fail "Please enter text" if (@contents != @contents.to_s)
+    fail "Please add some text" if @contents.empty?
   end
 
   def title
-    fail "Please enter text" if (@title != @title.to_s)
-    fail "Please add a title" if @title.empty?
     @title
   end
 
   def contents
-    fail "Please enter text" if (@contents != @contents.to_s)
-    fail "Please add some text" if @contents.empty?
     @contents
   end
 
   def count_words
-    if @contents.empty?
-     return 0
-    else 
-     @contents.split(" ").length
-     end
-   end      
+     @contents.split(" ").length   
+  end      
 
-  def reading_time(wpm) # wpm is an integer representing the number of words the
-                        # user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # for the contents at the given wpm.
+  def reading_time(wpm)
+    fail "wpm must be a number" if !wpm.is_a?(Numeric)
+    fail "wpm cannot be 0" if wpm == 0 
+    word_count = @contents.split(" ").length
+    minutes = word_count.to_f / wpm
+    minutes.round  
   end
 
-  def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
+  
+  def reading_chunk(wpm, minutes) 
+    chunksize = wpm * minutes
+    chunk = @contents.split(" ")[@start_at_word, @start_at_word + chunksize].join(" ")
+    @start_at_word += chunksize
+    chunk
+  end
+
+
+                                  # `wpm` is an integer representing the number
                                   # of words the user can read per minute
                                   # `minutes` is an integer representing the
                                   # number of minutes the user has to read
@@ -39,5 +47,5 @@ class DiaryEntry
     # If called again, `reading_chunk` should return the next chunk, skipping
     # what has already been read, until the contents is fully read.
     # The next call after that it should restart from the beginning.
-  end
+  
 end
--- spec/2-5_spec.rb
+++ spec/2-5_spec.rb
@@ -1,69 +1,118 @@
 require '2-5'
 
 RSpec.describe DiaryEntry do 
- # title tests 
+ # initialize tests 
   context "empty title returns error" do 
     it "asks for a title" do 
-      entry = DiaryEntry.new("", "Dear Diary, today I ate creams.")
-    expect{ entry.title }.to raise_error "Please add a title"
+    expect{ DiaryEntry.new("", "Dear Diary, today I ate creams.") }.to raise_error "Please add a title"
     end
   end
 
-    context "nontext title returns error" do 
-      it "asks for a title as text" do 
-        entry = DiaryEntry.new(6, "Dear Diary, today I ate creams.")
-      expect{ entry.title }.to raise_error "Please enter text"
-      end
+  context "nontext title returns error" do 
+    it "asks for a title as text" do 
+    expect{ entry = DiaryEntry.new(6, "Dear Diary, today I ate creams.") }.to raise_error "Please enter text"
     end
+  end
 
-    it "calling title returns the title" do 
-      entry = DiaryEntry.new("Tuesday", "Dear Diary, today I ate creams.")
-      result = entry.title 
-      expect(result).to eq "Tuesday"
+  context "nontext contents returns error" do 
+    it "asks for text" do 
+    expect{ DiaryEntry.new("Tuesday", 6) }.to raise_error "Please enter text"
     end
+  end
+
+  context "empty contents returns error" do 
+    it "asking for contents" do 
+      expect{ DiaryEntry.new("Tuesday", "") }.to raise_error "Please add some text"
+    end
+  end
+
+  # title tests 
+  it "calling title returns the title" do 
+    entry = DiaryEntry.new("Tuesday", "Dear Diary, today I ate creams.")
+    result = entry.title 
+    expect(result).to eq "Tuesday"
+  end
 
 # contents tests
-    context "nontext contents returns error" do 
-      it "asks for text" do 
-        entry = DiaryEntry.new("Tuesday", 6)
-      expect{ entry.contents }.to raise_error "Please enter text"
-      end
-    end
+  it "returns contents when called" do 
+    entry = DiaryEntry.new("Tuesday", "Dear Diary, today I ate creams.")
+    result = entry.contents 
+    expect(result).to eq "Dear Diary, today I ate creams."
+  end
+
+  # count_words tests
+  # no fail tests 
   
-    context "empty contents returns error" do 
-      it "asking for contents" do 
-        entry = DiaryEntry.new("Tuesday", "")
-        expect{ entry.contents }.to raise_error "Please add some text"
-      end
-    end
-
-    it "returns contents when called" do 
-      entry = DiaryEntry.new("Tuesday", "Dear Diary, today I ate creams.")
-      result = entry.contents 
-      expect(result).to eq "Dear Diary, today I ate creams."
-    end
-
-    # count_words tests
-    it "count_words returns 0 when given empty string" do
-      entry = DiaryEntry.new("Tuesday", "")
-      result = entry.count_words
-      expect(result).to eq 0
-    end
-    it "count_words returns 1 when given 1 word" do
-      entry = DiaryEntry.new("Tuesday", "foo")
-      result = entry.count_words
-      expect(result).to eq 1
-    end
-    it "count_words returns 10 when given 10 words" do
-      entry = DiaryEntry.new("Tuesday", "one two three four five six seven eight nine ten")
-      result = entry.count_words
-      expect(result).to eq 10
-    end
+  it "count_words returns 1 when given 1 word" do
+    entry = DiaryEntry.new("Tuesday", "foo")
+    result = entry.count_words
+    expect(result).to eq 1
+  end
+  it "count_words returns 10 when given 10 words" do
+    entry = DiaryEntry.new("Tuesday", "one two three four five six seven eight nine ten")
+    result = entry.count_words
+    expect(result).to eq 10
+  end
    
+  # reading time tests 
+
+  context "non integer supplied" do
+    it "prompts for integer" do 
+      entry = DiaryEntry.new("Tuesday", "one two")
+      expect{ entry.reading_time("two hundred") }.to raise_error "wpm must be a number"
+    end
+  end
+
+  context "wpm = 0" do
+    it "states wpm !=0" do 
+      entry = DiaryEntry.new("Tuesday", "one two")
+      expect{ entry.reading_time(0) }.to raise_error "wpm cannot be 0"
+    end
+  end
 
 
+  teststring = Array.new(200,"water").join(" ")
+  teststring2 = Array.new(100,"water").join(" ")
+  teststring3 = Array.new(550,"water").join(" ")
+  
+  it "outputs one minute for 200 words at 200wpm" do
+    entry = DiaryEntry.new("title", teststring)
+    result = entry.reading_time(200)
+    expect(result).to eq 1 
+  end
 
+  it "outputs one minute for 100 words at 200wpm" do
+    entry = DiaryEntry.new("title", teststring2)
+    result = entry.reading_time(200)
+    expect(result).to eq 1 
+  end
+
+  it "outputs 3 minutes for 550 words at 200wpm" do
+    entry = DiaryEntry.new("title", teststring3)
+    result = entry.reading_time(200)
+    expect(result).to eq 3 
+  end
+  
+  # reading chunk tests 
+  # 1. for 1 min should return wpm number of words (try wpm = eg. 1, 300)
+  # 2.1 to check next chunk, first chunk should not be part of second chunk 
+  # 2.2 if next chunk reaches end, it should stop at the end 
+  # 3. should output first chunk again after reaching the end 
+
+  it "for 1 min, outputs chunk of size = wpm" do 
+  entry = DiaryEntry.new("title", teststring3)
+  result = entry.reading_chunk(200, 1)
+  expect(result.split(" ").length).to eq 200
+  end
+  
+  # 2.1
+  it "subsequent chunk should not contain passage longer than 50 words of previous one" do
+    entry = DiaryEntry.new("title", teststring3)
 
 end
+
+
+
+
 
 
